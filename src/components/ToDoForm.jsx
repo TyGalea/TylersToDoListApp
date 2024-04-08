b4f43a02b44e302db9eb16f634e8557e7ba76528
TyGalea <galeaty@gmail.com>
2024-04-07 18:57:38 -0600

    lab 6 edit

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {TextInput, StyleSheet, Button, View} from 'react-native';
+import randomTasks from '../data/tasks';
 
 function ToDoForm({addTask}) {
   const [taskText, setTaskText] = React.useState('');
+  const [tasks, setTasks] = React.useState([]);
 
   const handlePress = taskText => {
     addTask(taskText);
     setTaskText('');
   };
+
+  const handleGenerateRandomTask = () => {
+    const randomTask =
+      tasks.tasks[Math.floor(Math.random() * tasks.tasks.length)];
+    setTaskText(randomTask);
+  };
+
+  useEffect(() => {
+    setTasks(randomTasks);
+  }, []);
 
   return (
     <>
@@ -19,6 +31,12 @@ function ToDoForm({addTask}) {
           value={taskText}
         />
         <Button title="Add Task" onPress={() => handlePress(taskText)} />
+      </View>
+      <View style={styles.random}>
+        <Button
+          title="Generate Random Task"
+          onPress={() => handleGenerateRandomTask()}
+        />
       </View>
     </>
   );
@@ -39,6 +57,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     marginRight: 10,
+  },
+  random: {
+    marginHorizontal: 20,
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 
